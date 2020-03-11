ae1300e8b9fb253c99fe3395b0f643ba1ee3ff1d
GBernardo10 <g_bernardo10@hotmail.com>
2020-03-11 02:45:52 -0300

    editor config

--- Preacher/src/index.js
+++ Preacher/src/index.js
@@ -3,14 +3,13 @@ const paht = require("path");
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
 const packageDefinition = protoLoader.loadSync(
-  paht.resolve(__dirname,"pb",'messages.proto'),
-  {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  }
+	paht.resolve(__dirname, "pb", "messages.proto"),
+	{
+		keepCase: true,
+		longs: String,
+		enums: String,
+		defaults: true,
+		oneofs: true
+	}
 );
 const proto = grpc.loadPackageDefinition(packageDefinition);
-console.log(proto)
