a94d1e63b8e67308c312b570f9c251beec9e4a81
GBernardo10 <g_bernardo10@hotmail.com>
2020-03-11 02:59:54 -0300

    index refactor

--- Preacher/src/index.js
+++ Preacher/src/index.js
@@ -1,15 +1,15 @@
-const paht = require("path");
+const path = require("path");
 
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
 const packageDefinition = protoLoader.loadSync(
-	paht.resolve(__dirname, "pb", "messages.proto"),
-	{
-		keepCase: true,
-		longs: String,
-		enums: String,
-		defaults: true,
-		oneofs: true
-	}
+  path.resolve(__dirname, "pb", "messages.proto"),
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
 );
 const proto = grpc.loadPackageDefinition(packageDefinition);
